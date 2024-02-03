677abd5ffab06af825ac9ca9cc6c57db4780194d
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-02-02 21:18:13 -0300

    Ajustado o resultado da opeção http

---
+++ GestaoDeProjetoWeb/Data/RetornoPaginadoGenerico.cs
@@ -0,0 +1,11 @@
+﻿namespace GestaoDeProjetoWeb.Data
+{
+    public class RetornoPaginadoGenerico<T>
+    {
+        public int ItemPorPagina { get; set; } = 30;
+        public int Pagina { get; set; } = 1;
+        public int TotalRegistros { get; set; } = 0;
+
+        public ICollection<T> Modelos { get; set; }
+    }
+}
