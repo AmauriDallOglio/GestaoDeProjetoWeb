47c0823b68da5ad97a1a95a938e21f4204ceb5ae
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-05-21 19:25:47 -0300

    Ajustes na tela de projeto

--- GestaoDeProjetoWeb/Data/ProjetoDto.cs
+++ GestaoDeProjetoWeb/Data/ProjetoDto.cs
@@ -7,12 +7,55 @@
         public string Descricao { get; set; }
         public DateTime DataHoraInicio { get; set; }
         public DateTime? DataHoraFim { get; set; }
-        public SituacaoProjeto Situacao { get; set; }
-        public string SituacaoDescricao => Situacao.ToString();
+        //public short Situacao { get; set; }
+
+        //public string SituacaoDescricao => Situacao.ToString();
+
+        // Propriedade Situacao com getter customizado
+        private short situacao;
+        public short Situacao
+        {
+            get { return situacao; }
+            set
+            {
+                situacao = value;
+                SituacaoDescricao = GetSituacaoDescricao(value);
+            }
+        }
+
+        public string SituacaoDescricao { get; private set; }
+
+
         public bool Selecionado { get; set; }
+
+        private string GetSituacaoDescricao(short situacao)
+        {
+            return situacao switch
+            {
+                1 => "Planejado",
+                2 => "Em Andamento",
+                3 => "Concluído",
+                4 => "Atrasado",
+                5 => "Em Revisão",
+                6 => "Em Espera",
+                7 => "Em Andamento (Baixa Prioridade)",
+                8 => "Em Andamento (Média Prioridade)",
+                9 => "Em Andamento (Alta Prioridade)",
+                10 => "Em Teste",
+                11 => "Pendente Aprovação",
+                12 => "Pendente Recursos",
+                13 => "Aguardando Feedback do Cliente",
+                14 => "Aguardando Aprovação Interna",
+                _ => "Desconhecido"
+            };
+        }
+
+
     }
 
-    public enum SituacaoProjeto : byte
+
+
+    public enum SituacaoProjeto : short
     {
         Planejado = 1,
         EmAndamento = 2,
